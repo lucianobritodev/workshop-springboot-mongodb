25edabf44086c75a933ddf16b7f6d9a9127952af
lucianobritodev <lucianobrito.dev@gmail.com>
2022-03-29 21:23:45 -0300

    Simple search with @Query

--- src/main/java/com/lucianobritodev/workshopspringbootmongodb/repositories/PostRepository.java
+++ src/main/java/com/lucianobritodev/workshopspringbootmongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucianobritodev.workshopspringbootmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucianobritodev.workshopspringbootmongodb.domain.Post;
@@ -10,6 +11,8 @@ import com.lucianobritodev.workshopspringbootmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
