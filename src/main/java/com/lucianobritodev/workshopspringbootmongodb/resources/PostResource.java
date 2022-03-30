ee411df1a42e76fe31e92394f53a6dc30642d1d5
lucianobritodev <lucianobrito.dev@gmail.com>
2022-03-29 21:13:55 -0300

    Simple search with query methods

--- src/main/java/com/lucianobritodev/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/lucianobritodev/workshopspringbootmongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.lucianobritodev.workshopspringbootmongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucianobritodev.workshopspringbootmongodb.domain.Post;
+import com.lucianobritodev.workshopspringbootmongodb.resources.util.URL;
 import com.lucianobritodev.workshopspringbootmongodb.services.PostService;
 
 @RestController
@@ -18,10 +22,25 @@ public class PostResource {
 	private PostService service;
 	
 
+	@GetMapping()
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(
+			@RequestParam(value="text", defaultValue="")  String text) {
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
